d4518e6a8f7e1caaf5aedf6bd6e21eadf8c1200c
Jonas Costa <jonascosta0299@gmail.com>
2025-02-02 21:45:01 -0300

    feat: add Custom Buttom component

--- src/components/custom-button/custom-buttom.component.tsx
+++ src/components/custom-button/custom-buttom.component.tsx
@@ -1,13 +1,18 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, ButtonHTMLAttributes } from 'react'
 
-import { CustomButtonContainer } from "./custom-buttom.styles"
+import { CustomButtonContainer, IconContainer } from "./custom-buttom.styles"
 
-interface Props {
+
+interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>  {
     children: React.ReactNode;
+    startIcon?: React.ReactNode;
 }
 
-const CustomButton: React.FC<Props> = ({ children }) => {
-    return <CustomButtonContainer>{children}</CustomButtonContainer>
+const CustomButton:FunctionComponent<CustomButtonProps> = ({ children, startIcon, ...rest }) => {
+    return <CustomButtonContainer {...rest}>
+        {startIcon && <IconContainer>{startIcon}</IconContainer>}
+        {children}
+        </CustomButtonContainer>
 }
 
 export default CustomButton
--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,3 +1,6 @@
+import { BsGoogle } from "react-icons/bs";
+
+import CustomButton from "../../components/custom-button/custom-buttom.component"
 import Header from "../../components/header/header.component"
 import { LoginContainer, LoginHeadline, LoginInputContainer, LoginSubtitle, LoginContent } from "./login.styles"
 
@@ -9,7 +12,7 @@ const LoginPage = () => {
             <LoginContent>
             <LoginHeadline>Entre com a sua conta</LoginHeadline>
 
-                {/* Button */}
+                <CustomButton startIcon={<BsGoogle size={18}/>}>Entrar com o google</CustomButton>
                 <LoginSubtitle>Ou entre com o seu e-mail</LoginSubtitle>
 
                 <LoginInputContainer>{/* Email Input */}</LoginInputContainer>
