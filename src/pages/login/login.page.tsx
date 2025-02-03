1ecdd6dd8455a25c37b54d7fdca7396855d8c20c
Jonas Costa <jonascosta0299@gmail.com>
2025-02-02 22:23:44 -0300

    feat: add custom input add to login page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -4,6 +4,7 @@ import {FiLogIn} from "react-icons/fi"
 //Components
 import CustomButton from "../../components/custom-button/custom-buttom.component"
 import Header from "../../components/header/header.component"
+import CustomInput from "../../components/custom-input/custom-input.component";
 
 ///Styles
 import { LoginContainer, LoginHeadline, LoginInputContainer, LoginSubtitle, LoginContent } from "./login.styles"
@@ -19,8 +20,12 @@ const LoginPage = () => {
                 <CustomButton startIcon={<BsGoogle size={18}/>}>Entrar com o google</CustomButton>
                 <LoginSubtitle>Ou entre com o seu e-mail</LoginSubtitle>
 
-                <LoginInputContainer>{/* Email Input */}</LoginInputContainer>
-                <LoginInputContainer>{/* Password Input */}</LoginInputContainer>
+                <LoginInputContainer>
+                    <CustomInput hasError={false} placeholder="Digite seu e-mail"></CustomInput>
+                </LoginInputContainer>
+                <LoginInputContainer>
+                    <CustomInput placeholder="Digite sua senha"></CustomInput>
+                </LoginInputContainer>
 
                 <CustomButton startIcon={<FiLogIn size={18}/>}>
                     Entrar
