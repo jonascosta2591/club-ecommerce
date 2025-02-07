c30d4b124810b5ce2cafd19286ffdf7a344fb48d
Jonas Costa <jonascosta0299@gmail.com>
2025-02-07 02:36:20 -0300

    feat: add input error message component

---
+++ src/components/input-error-message/input-error-message.component.tsx
@@ -0,0 +1,18 @@
+import {FunctionComponent} from 'react'
+
+//styles
+import { InputErrorMessageContainer } from './input-error-message.styles'
+
+interface InputErrorMessageProps {
+    children: React.ReactNode
+}
+
+const InputErrorMessage:FunctionComponent<InputErrorMessageProps> = ({children}) => {
+    return (
+        <>
+            <InputErrorMessageContainer>{children}</InputErrorMessageContainer>
+        </>
+    )
+}
+
+export default InputErrorMessage
