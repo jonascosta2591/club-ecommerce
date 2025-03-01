e10efd9d375b731dec1ee9598515ad3194c42d98
Jonas Costa <jonascosta0299@gmail.com>
2025-03-01 08:59:52 -0300

    chore: initialize firebase auth

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
+import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMyzfJg3NhJ7BWkn2xSqlCTOH3-8OntXc",
-  authDomain: "club-ecommerce-fca69.firebaseapp.com",
-  projectId: "club-ecommerce-fca69",
-  storageBucket: "club-ecommerce-fca69.firebasestorage.app",
-  messagingSenderId: "1003538205092",
-  appId: "1:1003538205092:web:bdab84db33f1de8a259931"
+  apiKey: "AIzaSyD2UJiEXQu_HmvVcWDK8U8Y5BsWzPEtOKg",
+  authDomain: "e-commercefsc.firebaseapp.com",
+  projectId: "e-commercefsc",
+  storageBucket: "e-commercefsc.firebasestorage.app",
+  messagingSenderId: "465044265014",
+  appId: "1:465044265014:web:2102ff305c67a287a3e097"
 };
 
 export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
+
+export const auth = getAuth()
